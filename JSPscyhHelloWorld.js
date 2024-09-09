94871732d152e8ca7aaac1fd8c98a08abc7e7b1b
Alissa McGill <alm161@zips.uakron.edu>
2024-09-09 01:00:45 -0400

    Test works

--- JSPscyhHelloWorld.js
+++ JSPscyhHelloWorld.js
@@ -1,4 +1,4 @@
-import Interactive from './dep/vector/dist/elements/interactive.js';
+//import Interactive from './dep/vector/dist/elements/interactive.js';
 // export utility functions
-export * from './dep/vector/dist/util/file.js';
-export * from './dep/vector/dist/util/math.js';
+//export * from './dep/vector/dist/util/file.js';
+//export * from './dep/vector/dist/util/math.js';
