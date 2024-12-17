5363b78dc05ea6067378e7a0b766b1bc4f6cdd2b
Vijay Marupudi <vijay@vijaymarupudi.com>
2024-12-17 14:33:18 -0500

    My edits

---
+++ ideal-main.js
@@ -0,0 +1,13 @@
+function numberLineEstimationTrial(el, trial, callback) {
+  document.body.innerHTML = `
+<div id="number-line"></div>
+`
+
+  // your code
+}
+
+numberLineEstimationTrial(document.body, {}, (data) => {
+  console.log(data)
+})
+
+
